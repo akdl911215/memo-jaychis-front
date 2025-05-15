0a7d4d80a8deb5533457150aaab84067b0cfb83e
akdl911215 <akdl911215@naver.com>
2025-05-15 13:09:56 +0900

    update: next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    appDir: true,
+  },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
